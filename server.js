35b792274442db4ee2bab6d8ddfbacf011ee2c68
Samuel Kingsley <kins4swagg@gmail.com>
2020-02-07 14:20:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,125 +1,132 @@
-//  OpenShift sample Node application
-var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
-
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
-
+var express = require('express');
+var app = express();
+var fs = require("fs");
+const forge    = require('node-forge');
+const request = require('request-promise-native');
+const md5 = require('md5');
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
+app.post('/pay/', function (req, res) {
+//    console.log("This is where the action will happen ");
+   var rave = new Rave(req.body.PBFPubKey,req.body.seckey);
+ rave.initiatePayment({
+    "cardno": req.body.cardno,
+    "cvv": req.body.cvv,
+    "expirymonth": req.body.expirymonth,
+    "expiryyear": req.body.expiryyear,
+    "currency": req.body.currency,
+    "pin": req.body.pin,
+    "country": req.body.country,
+    "amount": req.body.amount,
+    "email": req.body.email,
+    "suggested_auth": "PIN",
+    "phonenumber": req.body.phonenumber,
+    "firstname": req.body.firstname,
+    "lastname": req.body.lastname,
+    "IP": "783249238738",
+    "txRef": "MC-" + Date.now(),
+    "redirect_url": "http://tekplugin.com/transactionSuccessful.html",
+    "device_fingerprint": req.body.device_fingerprint
+  }).then(function(result) {
+      console.log(result);
 
-if (mongoURL == null) {
-  var mongoHost, mongoPort, mongoDatabase, mongoPassword, mongoUser;
-  // If using plane old env vars via service discovery
-  if (process.env.DATABASE_SERVICE_NAME) {
-    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
-    mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'];
-    mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'];
-    mongoDatabase = process.env[mongoServiceName + '_DATABASE'];
-    mongoPassword = process.env[mongoServiceName + '_PASSWORD'];
-    mongoUser = process.env[mongoServiceName + '_USER'];
-
-  // If using env vars from secret from service binding  
-  } else if (process.env.database_name) {
-    mongoDatabase = process.env.database_name;
-    mongoPassword = process.env.password;
-    mongoUser = process.env.username;
-    var mongoUriParts = process.env.uri && process.env.uri.split("//");
-    if (mongoUriParts.length == 2) {
-      mongoUriParts = mongoUriParts[1].split(":");
-      if (mongoUriParts && mongoUriParts.length == 2) {
-        mongoHost = mongoUriParts[0];
-        mongoPort = mongoUriParts[1];
-      }
-    }
+    res.send(result);
   }
-
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-  }
+ ).catch(error => res.send(error));
 }
-var db = null,
-    dbDetails = new Object();
+)
 
-var initDb = function(callback) {
-  if (mongoURL == null) return;
+// var server = app.listen(80, function () {
+//    var host = server.address().address
+//    var port = server.address().port
+//    console.log("Listening on port 80");
+// //    console.log("Example app listening at, host, port)
+// })
 
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
-
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
-
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
-
-    console.log('Connected to MongoDB at: %s', mongoURL);
-  });
-};
-
-app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      if (err) {
-        console.log('Error running count. Message:\n'+err);
-      }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
-});
-
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
-});
-
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
-
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
+// var server=app.listen(process.env.PORT || 4000, function(){
+//   console.log('Your node js server is running');
+// });
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 
-module.exports = app ;
+
+
+
+
+
+
+var options = {
+	url: "",
+	method: "",
+	headers: {
+		'Content-Type': 'application/json',
+		'Accept': 'application/json'
+	},
+	body: {
+		"PBFPubKey": "",
+		"alg": "3DES-24",
+		client: "",
+	},
+	json: true
+}
+
+class Rave {
+	/**
+	 * Rave object constructor
+	 * @param {*} public_key This is a string that can be found in merchant rave dashboard
+	 * @param {*} secret_key This is a string that can be found in merchant rave dashboard
+	 */
+	constructor(public_key, secret_key){
+        this.public_key = public_key;
+        this.secret_key = secret_key;
+    }
+
+	encryptCardDetails(key,card_details) {
+        card_details = JSON.stringify(card_details);
+        let cipher   = forge.cipher.createCipher('3DES-ECB', forge.util.createBuffer(key));
+        cipher.start({iv:''});
+        cipher.update(forge.util.createBuffer(card_details, 'utf-8'));
+        cipher.finish();
+        let encrypted = cipher.output;
+        return ( forge.util.encode64(encrypted.getBytes()) );
+	}
+
+	getKey() {
+         let sec_key = this.secret_key;
+        let keymd5 = md5(sec_key);
+        let keymd5last12 = keymd5.substr(-12);
+
+        let seckeyadjusted = sec_key.replace('FLWSECK-', '');
+        let seckeyadjustedfirst12 = seckeyadjusted.substr(0, 12);
+
+        return seckeyadjustedfirst12 + keymd5last12;
+	}
+
+	initiatePayment(card_details) {
+		return new Promise((resolve, reject) => {
+            let hashed_key = this.getKey();
+			let encrypted_card_details = this.encryptCardDetails(hashed_key,card_details);
+			let payment_options = Object.assign({}, options);
+			payment_options.url = 'https://api.ravepay.co/flwv3-pug/getpaidx/api/charge';
+			payment_options.body.client = encrypted_card_details;
+            payment_options.method = 'POST';
+            payment_options.body.PBFPubKey = this.public_key; // set public key
+            // console.log(encrypted_card_details);
+
+			request(payment_options)
+				.then((result) => {
+                    resolve(result);
+                    // console.log(result);
+				}).catch((err) => {
+         reject(err);
+                    // console.log(err);
+				});
+			})
+	}
+}
